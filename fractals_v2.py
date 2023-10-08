e9c0e89f3e2426546b211032f5449b64db9c91ab
yanbinma <36197731+yanbinma@users.noreply.github.com>
2023-10-08 10:43:40 -0400

    Create fractals_v2.py
    This version focuses on exploring the effects of exponents on the resulted fractal shape. It's clear that the number of vertices is exactly one degree lower than the exponent used in the Mandelbrot formula.

---
+++ fractals_v2.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#v2 focuses on the effects with changing exponent values
+num=200
+tests=125
+power=2
+# test 'tests' times, starting from 0 
+def isMandelbrotSet(cn):
+    z = 0
+    for i in range(tests):
+        z = z**power + cn
+        if abs(z.real)>2.0 or abs(z.imag)>2.0:
+            return False
+    return True
+
+# given a rectangle, draw in the area
+def calcCoordinatesInRect(x1,x2,y1,y2):
+    xlist=[]
+    ylist=[]
+    A=np.linspace(x1, x2, num)
+    B=np.linspace(y1, y2, num)
+    for x in range(0, num):
+        for y in range(0, num):
+            if isMandelbrotSet(complex(A[x],B[y])):
+                xlist.append(A[x])
+                ylist.append(B[y])
+    return xlist, ylist                
+
+def drawInRect(x1,x2,y1,y2):
+    xlist, ylist = calcCoordinatesInRect(x1,x2,y1,y2)
+    plt.scatter(xlist, ylist)
+
+# test all complex numbers within (-2:2, -2:2)
+plt.ion()
+for power in range(2, 100):
+    plt.clf()
+    drawInRect(-2, 2, -2, 2)
+    plt.title("Mandelbrot Exponent: " + str(power))
+    plt.gcf().canvas.draw()
+    plt.gcf().canvas.flush_events()
+    plt.pause(1)
