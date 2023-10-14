85f5fc6044c1a2fee26b20621b5a45890b8477d1
yanbinma <yanbin_ma@yahoo.com>
2023-10-13 20:43:08 -0400

    added color

--- fractals_v1.py
+++ fractals_v1.py
@@ -1,37 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 num=200
 # one interesting find: the resulted shape seems to be power-1 sided;
 # when power=6, it's shaped as a pentagon; when zoom-in, it starts to look like human palm with 5 fingers 
-power=6
+power=2
 tests=125
 toShow=False
+# rainbow colors
+rainbow_colors = ['#0000FF', '#00FF00', '#FFFF00', '#FF7F00', '#FF2600', '#FF3500', '#FF4000', '#ff5100', '#ffac00', '#fff100', '#0bff00', '#00f6ff', '#233067']
+cfactor = math.ceil(tests * 1.0 / len(rainbow_colors))
 # test 'tests' times, starting from 0 
 def isMandelbrotSet(cn):
     z = 0
     for i in range(tests):
         z = z**power + cn
         if abs(z.real)>2.0 or abs(z.imag)>2.0:
-            return False
-    return True
+            return i
+    return 0
 
 # given a rectangle, draw in the area
 def calcCoordinatesInRect(x1,x2,y1,y2):
     xlist=[]
     ylist=[]
+    clist=[]
     A=np.linspace(x1, x2, num)
     B=np.linspace(y1, y2, num)
     for x in range(0, num):
         for y in range(0, num):
-            if isMandelbrotSet(complex(A[x],B[y])):
-                xlist.append(A[x])
-                ylist.append(B[y])
-    return xlist, ylist                
+            ccode = isMandelbrotSet(complex(A[x],B[y]))
+            xlist.append(A[x])
+            ylist.append(B[y])
+            clist.append(ccode % cfactor)
+    return xlist, ylist, clist                
 
 def drawInRect(x1,x2,y1,y2):
-    xlist, ylist = calcCoordinatesInRect(x1,x2,y1,y2)
-    plt.scatter(xlist, ylist)
+    xlist, ylist, clist = calcCoordinatesInRect(x1,x2,y1,y2)
+    plt.scatter(xlist, ylist, c=clist)
     if toShow:
         plt.show()
 
